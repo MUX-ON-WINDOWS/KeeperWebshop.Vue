483147a7193fe3249d150cebc4e7c314c31fd2ec
Max <maxnouts@gmail.com>
2023-05-09 13:27:18 +0200

    added count function.

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -5,12 +5,34 @@ import { RouterLink } from 'vue-router'
 <template>
   <header>
     <nav>
-      <RouterLink to="/"><img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /></RouterLink>
+      <RouterLink to="/"><img alt="Vue logo" class="logo" src="@/assets/logo.png" width="125" height="125" /></RouterLink>
       <RouterLink to="/about">Sale</RouterLink>
       <RouterLink to="/handschoenen">Handschoenen</RouterLink>
       <RouterLink to="/onderhoud">Onderhoud</RouterLink>
-      <RouterLink to="/about">Winkelwagen</RouterLink>
+      <div class="cart_container" @click="counter">
+        <div class="amount_cart">{{ count || 0 }}</div>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+        </svg>
+      </div>
     </nav>
   </header>
 </template>
-
+<script>
+  export default {
+    data() {
+      return {
+        count: -1
+      }
+    },
+    methods: {
+      counter() {
+        this.count++
+      }
+    },
+    mounted() {
+      // methods can be called in lifecycle hooks, or other methods!
+      this.counter()
+    }
+  }
+</script>
